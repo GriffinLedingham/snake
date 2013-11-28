970c87dd02d1b2715d2d9eed6703483407ba4cb2
GriffinLedingham <gcledingham@gmail.com>
2013-11-28 08:50:33 -0800

    [griff] Laying initial ground work for server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+var http = require('http');
+var express = require('express');
+var app = express()
+
+app.use(express.static("client/public"));
+
+var server = http.createServer(app);
+
+//app.listen(8142);
+
+var io = require('socket.io').listen(server);
+
+server.listen(8142);
